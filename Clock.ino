db4a98d59237afe25ffc7607f6e0fc530c542717
Andrewishere502 <35299674+Andrewishere502@users.noreply.github.com>
2023-09-09 16:58:53 -0500

    Update Clock.ino
    Added ability to adjust time manually

--- Clock.ino
+++ Clock.ino
@@ -1,7 +1,7 @@
 /*
  * Author: Andrew Berntson
  * Date Created: August 11 2023
- * Date Updated: August 20 2023
+ * Date Updated: September 9 2023
  */
 
 // Define output pins for 74HC595
@@ -15,14 +15,23 @@
 #define enable3 D6  // Third digit
 #define enable4 D7  // Fourth digit
 
-// Start time for clock
-const int start_hour = 5;
-const int start_minute = 58;
-const int start_second = 0;
-// Hour and minute outputs for display, as well as seconds
+// Define pin for reading input for time changing
+#define toggle D3 // Toggle between adjusting hours or minutes
+#define button D8 // Increment hours/minutes by 1
+
+// Parameters for pressing the time-adjust button
+int last_pressed = 0;         // last time (in ms since starting) button was pressed
+const int button_delay = 200; // in milliseconds
+
+// Time offsets for adjusting clock's time
+int offset_h = 16; // Offset for hours
+int offset_m = 30; // Offset for minutes
+int offset_s = 0; // Offset for seconds
+
+// Hour, minute, second outputs for display
 int hours = 0;
 int minutes = 0;
-int seconds = 0;
+int seconds = 0; // not displayed
 
 // Shift out a byte from the shift register
 void sendByte(byte data) {
@@ -136,7 +145,7 @@ byte encode(char c) {
 }
 
 // Output 4 ints to the display
-void update_display(int dig1, int dig2, int dig3, int dig4) {
+void displayNum(int dig1, int dig2, int dig3, int dig4) {
   // Set the first digit
   sendByte(encode(dig1));
   enableDigit(1); // Turn off all digits but the 1st
@@ -159,7 +168,7 @@ void update_display(int dig1, int dig2, int dig3, int dig4) {
 }
 
 // Output 4 chars to the display, message must be lowercase
-void write_message(char message[4]) {
+void displayStr(char message[4]) {
   // Display the first character in the first digit
   sendByte(encode(message[0]));
   enableDigit(1); // Turn off all digits but the 1st
@@ -181,18 +190,41 @@ void write_message(char message[4]) {
   delay(1); // Delay makes multiplexing possible
 }
 
-// Update the clock's time (hours and minutes)
-void update_time() {
-  // Calculate the clock second
-  seconds = (start_second + millis() / 1000) % 60;
-  // Calculate the clock minute
-  minutes = (start_minute + (start_second + millis() / 1000) / 60) % 60;
-  // Calculate the clock hour
-  hours = (start_hour + (start_minute + (start_second + millis() / 1000) / 60) / 60) % 24;
+// Return the seconds on the clock.
+int getSeconds() {
+  return (offset_s + millis() / 1000) % 60;
+}
+
+// Return the minutes on the clock.
+int getMinutes() {
+  return (offset_m + (offset_s + millis() / 1000) / 60) % 60;
+}
+
+// Return the hours on the clock.
+int getHours() {
+  return (offset_h + (offset_m + (offset_s + millis() / 1000) / 60) / 60) % 24;
+}
+
+// Adjust offsets if the button was pressed.
+void adjustOffsets() {
+  // When the button is pressed, check which part of the
+  // time to increment: Hours or minutes. Provided they didn't
+  // press the button less than 200ms since they last pressed it
+  //
+  // toggle == HIGH for hours
+  // toggle == LOW for minutes
+  if (digitalRead(button) == HIGH && last_pressed + button_delay <= millis()) {
+    if (digitalRead(toggle) == HIGH) {  // Increment hours by 1
+      offset_h = (offset_h + 1) % 24;
+    } else {                            // Increment minutes by 1
+      offset_m = (offset_m + 1) % 60;
+    }
+    last_pressed = millis();
+  }
 }
 
 void setup() {
-  // Set all pins to output
+  // Set pins to output
   pinMode(RCLK, OUTPUT);
   pinMode(SER, OUTPUT);
   pinMode(SRCLK, OUTPUT);
@@ -200,20 +232,31 @@ void setup() {
   pinMode(enable2, OUTPUT);
   pinMode(enable3, OUTPUT);
   pinMode(enable4, OUTPUT);
+
+  // Set pins to input
+  pinMode(toggle, INPUT_PULLUP);
+  pinMode(button, INPUT);
 }
 
 void loop() {
-  // Update the clock's internal time
-  update_time();
+  // Adjust offsets to change the time
+  adjustOffsets();
+
+  // Update the time on the clock
+  seconds = getSeconds();
+  minutes = getMinutes();
+  hours = getHours();
 
   // Update the clock's display with a super cool secret
   // message, or just the boring time.
   if (hours == 0 && minutes < 1) {         // Display message for 1 min at 12am
-    write_message("poo ");
+    displayStr("poo ");
   } else if (hours == 12 && minutes < 1) { // Display message for 1 min at 12pm
-    write_message("pee ");
+    displayStr("pee ");
   } else {
     // Update the clock's display with the time
-    update_display(hours / 10, hours % 10, minutes / 10, minutes % 10);
+    displayNum(hours / 10, hours % 10, minutes / 10, minutes % 10);
   }
+
+  
 }
